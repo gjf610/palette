4f38332baae5cce0bd2713b5912a1055346a9ed9
jianfeng <gjf610@yeah.net>
2021-12-13 21:01:16 +0800

    feat: draw line

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log('hi')
+
+
+let canvas = document.getElementById('canvas')
+canvas.width = document.documentElement.clientWidth
+canvas.height = document.documentElement.clientHeight
+let ctx = canvas.getContext('2d')
+
+let painting = false
+ctx.fillStyle = "black";
+ctx.strokeStyle = 'none';
+
+
+canvas.onmousedown = () => {
+    painting = true
+}
+
+// draw line
+canvas.onmousemove = (e) => {
+    if (painting === true) {
+        ctx.beginPath();
+        ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI)
+        ctx.stroke();
+        ctx.fill();
+    }
+
+}
+canvas.onmouseup = () => {
+    painting = false
+}
