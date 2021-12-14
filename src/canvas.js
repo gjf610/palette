20eac5bd1bf7011dc9b37647801592892f737e89
jianfeng <gjf610@yeah.net>
2021-12-14 10:11:06 +0800

    refactor: MVC

--- src/canvas.js
+++ src/canvas.js
@@ -5,137 +5,144 @@ function getPos(ev) { //获取鼠标位置封装成函数
     var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
     return { x: ev.pageX + scrollLeft - 20, y: ev.pageY + scrollTop - 70 }; //JSON形式返回
 }
-function is_touch_device() {
-    return 'ontouchstart' in window;
-}
-window.onload = function () {
-    let canvas = document.getElementById('canvas');
-    canvas.width = document.documentElement.clientWidth;
-    canvas.height = document.documentElement.clientHeight;
 
-
-
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'black';
-    ctx.strokeStyle = '#000';
-    let curColor = ctx.strokeStyle;
-    ctx.lineWidth = 12;
-    ctx.lineCap = "round";
-    let isPainting = false;
-    let last = null;
-    //判断是否是触屏设备
-
-
-    function draw(x1, y1, x2, y2) {
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
+const m = {
+    data: {
+        isPainting: false,
+        last: null,
+        isTouchDevice: 'ontouchstart' in window,
     }
-    if (is_touch_device()) {
-        //触摸设备
-        canvas.ontouchstart = (e) => {
-            last = [e.touches[0].clientX - 20, e.touches[0].clientY - 70]
-        }
-        canvas.ontouchmove = (e) => {
-            e.preventDefault();
-            draw(last[0], last[1], e.touches[0].clientX - 20, e.touches[0].clientY - 70);
-            last = [e.touches[0].clientX - 20, e.touches[0].clientY - 70]
-        }
-    } else {
-        //鼠标
-        canvas.onmousedown = (e) => {
-            var pos = getPos(e);
-            isPainting = true;
-            last = [pos.x, pos.y];
-        }
-        canvas.onmousemove = (e) => {
-            var pos = getPos(e);
+}
+const v = {
+    canvas: document.getElementById('canvas'),
+    ctx: null,
+    init() {
+        this.ctx = v.canvas.getContext('2d')
+        this.ctx.fillStyle = 'black';
+        this.ctx.strokeStyle = '#000';
+        this.ctx.lineWidth = 12;
+        this.ctx.lineCap = "round";
+    },
+    draw(x1, y1, x2, y2) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(x1, y1);
+        this.ctx.lineTo(x2, y2);
+        this.ctx.stroke();
+    }
+}
+const c = {
+    init() {
+        v.canvas.width = document.documentElement.clientWidth;
+        v.canvas.height = document.documentElement.clientHeight;
+        v.init()
 
-            if (isPainting) {
-                draw(last[0], last[1], pos.x, pos.y);
-                last = [pos.x, pos.y];
+        let curColor = v.ctx.strokeStyle;
+
+        if (m.data.isTouchDevice) {
+            //触摸设备
+            v.canvas.ontouchstart = (e) => {
+                m.data.last = [e.touches[0].clientX - 20, e.touches[0].clientY - 70]
+            }
+            v.canvas.ontouchmove = (e) => {
+                e.preventDefault();
+                v.draw(m.data.last[0], m.data.last[1], e.touches[0].clientX - 20, e.touches[0].clientY - 70);
+                m.data.last = [e.touches[0].clientX - 20, e.touches[0].clientY - 70]
+            }
+        } else {
+            //鼠标
+            v.canvas.onmousedown = (e) => {
+                var pos = getPos(e);
+                m.data.isPainting = true;
+                m.data.last = [pos.x, pos.y];
+            }
+            v.canvas.onmousemove = (e) => {
+                var pos = getPos(e);
+
+                if (m.data.isPainting) {
+                    v.draw(m.data.last[0], m.data.last[1], pos.x, pos.y);
+                    m.data.last = [pos.x, pos.y];
+                }
+            }
+            v.canvas.onmouseup = () => {
+                m.data.isPainting = false
             }
         }
-        canvas.onmouseup = () => {
-            isPainting = false
+        //navBar
+        let colorBtn = document.querySelector('#penColor');
+        let colorChoseDiv = document.querySelector('#penColorChose');
+        let sizeBtn = document.querySelector('#penSize');
+        let sizeChoseDiv = document.querySelector('#penSizeChose');
+        let penEraser = document.querySelector('#penEraser');
+        let clearBtn = document.querySelector('#penClear');
+        document.onclick = () => {
+            console.log(startMove)
+            startMove(colorChoseDiv, {
+                height: 0
+            });
+            startMove(sizeChoseDiv, {
+                height: 0
+            });
+        }
+        colorBtn.onclick = function (e) {
+            startMove(colorChoseDiv, {
+                height: 140
+            });
+            e.cancelBubble = true;
+        }
+        sizeBtn.onclick = function (e) {
+            v.ctx.strokeStyle = curColor;
+            sizeBtn.classList.add('navActive');
+            penEraser.classList.remove('navActive');
+            startMove(sizeChoseDiv, {
+                height: 140
+            });
+            e.cancelBubble = true;
+        }
+        penEraser.onclick = function (e) {
+            penEraser.classList.add('navActive');
+            sizeBtn.classList.remove('navActive');
+            v.ctx.strokeStyle = '#fff';
+        }
+        clearBtn.onclick = function () {
+            v.ctx.clearRect(0, 0, v.canvas.width, v.canvas.height);
+        }
+
+        //颜色的切换
+        let redBtn = colorChoseDiv.querySelector('.colorRed');
+        let greenBtn = colorChoseDiv.querySelector('.colorGreen');
+
+        let blueBtn = colorChoseDiv.querySelector('.colorBlue');
+        let blackBtn = colorChoseDiv.querySelector('.colorBlack');
+        redBtn.onclick = function () { //切换红色
+            v.ctx.strokeStyle = 'rgb(255,64,0)';
+            curColor = v.ctx.strokeStyle;
+        }
+        greenBtn.onclick = function () {//切换绿色
+            v.ctx.strokeStyle = 'rgb(74,222,149)'
+        }
+        blueBtn.onclick = function () { //切换蓝色
+            v.ctx.strokeStyle = 'rgb(0,68,255)';
+            curColor = v.ctx.strokeStyle;
+        }
+        blackBtn.onclick = function () { //切换黑色
+            v.ctx.strokeStyle = '#000';
+            curColor = v.ctx.strokeStyle;
+        }
+        //尺寸的切换
+        let sSizeBtn = sizeChoseDiv.querySelector('.sSize');
+        let mSizeBtn = sizeChoseDiv.querySelector('.mSize');
+        let lSizeBtn = sizeChoseDiv.querySelector('.lSize');
+        sSizeBtn.onclick = function () {
+            v.ctx.lineWidth = 7;
+        }
+        mSizeBtn.onclick = function () {
+            v.ctx.lineWidth = 12;
+        }
+        lSizeBtn.onclick = function () {
+            v.ctx.lineWidth = 17;
         }
     }
-    //navBar
-    let colorBtn = document.querySelector('#penColor');
-    let colorChoseDiv = document.querySelector('#penColorChose');
-    let sizeBtn = document.querySelector('#penSize');
-    let sizeChoseDiv = document.querySelector('#penSizeChose');
-    let penEraser = document.querySelector('#penEraser');
-    let clearBtn = document.querySelector('#penClear');
-    document.onclick = () => {
-        console.log(startMove)
-        startMove(colorChoseDiv, {
-            height: 0
-        });
-        startMove(sizeChoseDiv, {
-            height: 0
-        });
-    }
-    colorBtn.onclick = function (e) {
-        startMove(colorChoseDiv, {
-            height: 140
-        });
-        e.cancelBubble = true;
-    }
-    sizeBtn.onclick = function (e) {
-        ctx.strokeStyle = curColor;
-        sizeBtn.classList.add('navActive');
-        penEraser.classList.remove('navActive');
-        startMove(sizeChoseDiv, {
-            height: 140
-        });
-        e.cancelBubble = true;
-    }
-    penEraser.onclick = function (e) {
-        penEraser.classList.add('navActive');
-        sizeBtn.classList.remove('navActive');
-        ctx.strokeStyle = '#fff';
-    }
-    clearBtn.onclick = function () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-
-    //颜色的切换
-    let redBtn = colorChoseDiv.querySelector('.colorRed');
-    let greenBtn = colorChoseDiv.querySelector('.colorGreen');
-
-    let blueBtn = colorChoseDiv.querySelector('.colorBlue');
-    let blackBtn = colorChoseDiv.querySelector('.colorBlack');
-    redBtn.onclick = function () { //切换红色
-        ctx.strokeStyle = 'rgb(255,64,0)';
-        curColor = ctx.strokeStyle;
-    }
-    greenBtn.onclick = function () {//切换绿色
-        ctx.strokeStyle = 'rgb(74,222,149)'
-    }
-    blueBtn.onclick = function () { //切换蓝色
-        ctx.strokeStyle = 'rgb(0,68,255)';
-        curColor = ctx.strokeStyle;
-    }
-    blackBtn.onclick = function () { //切换黑色
-        ctx.strokeStyle = '#000';
-        curColor = ctx.strokeStyle;
-    }
-    //尺寸的切换
-    let sSizeBtn = sizeChoseDiv.querySelector('.sSize');
-    let mSizeBtn = sizeChoseDiv.querySelector('.mSize');
-    let lSizeBtn = sizeChoseDiv.querySelector('.lSize');
-    sSizeBtn.onclick = function () {
-        ctx.lineWidth = 7;
-    }
-    mSizeBtn.onclick = function () {
-        ctx.lineWidth = 12;
-    }
-    lSizeBtn.onclick = function () {
-        ctx.lineWidth = 17;
-    }
-
 }
+c.init()
 
 
